98261247d73feb6a98dc88b23871476a3e2cccfa
Bryan Felipe Packer <115124886+BryanPacker@users.noreply.github.com>
2025-10-21 21:47:11 -0300

    Change AMI to Amazon Linux 2023 and update subnet
    Updated the AMI to Amazon Linux 2023 and modified subnet configuration. Removed output for public IP and DNS.

--- main.tf
+++ main.tf
@@ -58,17 +58,18 @@ resource "aws_key_pair" "debian_key" {
   public_key = file("~/.ssh/id_rsa.pub")  # Altere para o caminho da sua chave pública
 }
 resource "aws_instance" "debian" {
-  ami           = data.aws_ami.debian.id
+  ami           = ami = data.aws_ami.amazon_linux_2023.id
   instance_type = "t3.micro" 
   key_name               = aws_key_pair.debian_key.key_name
   vpc_security_group_ids = [aws_security_group.debian_sg.id]
-  subnet = subnet-08ab1cc11d069cf59
+  subnet_id = "subnet-08ab1cc11d069cf59"
   associate_public_ip_address = true
   user_data = <<-EOF
               #!/bin/bash
               yum update -y ; yum install -y git 
               systemctl enable git ; systemctl start git
               git clone https://github.com/BryanPacker/proway-docker.git
+              EOF
   tags = {
     Name = "debian-instance"
   }
@@ -77,16 +78,6 @@ resource "aws_instance" "debian" {
 output "instance_id" {
   description = "ID da instância EC2"
   value       = aws_instance.debian.id
-}
-
-output "instance_public_ip" {
-  description = "IP público da instância"
-  value       = aws_eip.debian_eip.public_ip
-}
-
-output "instance_public_dns" {
-  description = "DNS público da instância"
-  value       = aws_instance.debian.public_dns
 }
 
 output "ssh_connection" {
